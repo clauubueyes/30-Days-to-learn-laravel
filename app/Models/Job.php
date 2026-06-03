3c3abc40029bd8621fd7f1f372291ab8038f4ba9
clauubuyeyes <clauubueyes@outlook.com>
2026-06-03 11:29:10 +0200

    day 10

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,30 +2,12 @@
 
 namespace App\Models;
 use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Job{ 
-    public static function all():array{
-         return [ 
-            [
-                'id' => 1,
-                'title' => 'Co-Fundadora & CTO de AppForo',
-                'salary' => 'Estoy de gratis :('
-            ] , 
-            [
-                'id' =>  2,
-                'title' => 'Desarrolladora de Everlasting',
-            'salary' => 'Estoy de gratis tambien :('
-            ] 
-        ];
-    }
-
-    public static function find(int $id):array{
-         $job =  Arr :: first(static::All(),fn($job) => $job['id'] == $id);
-
-         if(! $job){
-            abort(404);
-         }
-         
-         return $job;
-    }
+class Job extends Model{ 
+    use HasFactory;
+    protected $table = 'jobs_listings';
+    protected $fillable = ['title', 'salary'];
+   
 }
